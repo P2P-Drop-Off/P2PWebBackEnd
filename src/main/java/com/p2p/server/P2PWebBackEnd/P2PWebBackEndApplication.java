799e79159de3340f6ea69e92237c7107754e243f
kaiwm <kaiwm@uci.edu>
2026-01-14 16:24:57 -0800

    hi

---
+++ src/main/java/com/p2p/server/P2PWebBackEnd/P2PWebBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.p2p.server.P2PWebBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2PWebBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2PWebBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p2p/server/P2PWebBackEnd/P2PWebBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2p.server.P2PWebBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2PWebBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
